68cf521634122722d23b7be24cc9da972870f14b
Shark090 <eye3692@gmail.com>
2020-04-19 14:41:32 +0900

    DBServer 전체 송신

--- ChatClientCS/DB/HTTPClientForDB.cs
+++ ChatClientCS/DB/HTTPClientForDB.cs
@@ -28,7 +28,7 @@ namespace ChatClientCS
             DBserver2 = consoleIni.DBserver2 + "/u2sns.aspx?";
         }
 
-        public void DB(string sender, string receiver, string msg, string filename = "")
+        public void DB_MainSub(string sender, string receiver, string msg, string filename = "")
         {
             using (var client = new MyWebClient(3))
             {
@@ -88,5 +88,72 @@ namespace ChatClientCS
                 }
             }
         }
+
+        public void DB(string sender, string receiver, string msg, string filename = "")
+        {
+            using (var client = new MyWebClient(3))
+            {
+                var values = new NameValueCollection();
+                values["HBTo"] = sender;
+                values["HBFrom"] = receiver;
+                values["HBMemo"] = msg;
+                values["HBFile"] = filename;
+
+                int e_flag = 0;
+
+                try
+                {
+                    var response = client.UploadValues(DBserver1, values);
+                    var responseString = Encoding.Default.GetString(response);
+                    //Console.WriteLine(responseString);
+                }
+                catch (Exception e)
+                {
+                    string strFileName = DateTime.Today.ToString("yyyy-MM-dd") + "_DB Error.txt";
+                    string strDate = DateTime.Now.ToString("[HH:mm:ss]");
+                    string strData = "";
+
+                    strTemp = DBserver1.Replace("/u2sns.aspx?", "");
+                    strData = "DB서버(" + strTemp + ")가 비가용 상태입니다. " + e.Message;
+                    //Console.WriteLine(strData);
+                    log.WriteDBFile(strFileName, strDate, strData);
+
+                    e_flag++;
+                }
+
+                try
+                {
+                    var response = client.UploadValues(DBserver2, values);
+                    var responseString = Encoding.Default.GetString(response);
+                    //Console.WriteLine(responseString);
+                }
+                catch (Exception e)
+                {
+                    string strFileName = DateTime.Today.ToString("yyyy-MM-dd") + "_DB Error.txt";
+                    string strDate = DateTime.Now.ToString("[HH:mm:ss]");
+                    string strData = "";
+
+                    strTemp = DBserver2.Replace("/u2sns.aspx?", "");
+                    strData = "DB서버(" + strTemp + ")가 비가용 상태입니다. " + e.Message;
+                    //Console.WriteLine(strData);
+                    log.WriteDBFile(strFileName, strDate, strData);
+
+                    e_flag++;
+                }
+
+                if(e_flag == 2)
+                {
+                    string strFileName = DateTime.Today.ToString("yyyy-MM-dd") + "_DB Error.txt";
+                    string strDate = DateTime.Now.ToString("[HH:mm:ss]");
+                    string strData = "";
+
+                    strData = "소초내 모든DB 서버가 비가용 상태입니다. 메시지를 저장할 수 없습니다.";
+                    //Console.WriteLine(strData);
+                    log.WriteDBFile(strFileName, strDate, strData);
+                }
+            }
+        }
+
+
     }
 }
